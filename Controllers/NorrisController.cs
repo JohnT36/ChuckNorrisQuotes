f46b3fdb76418852bcaa01656a4324f283bb0c6d
John Tuck <j.tuck1336@gmail.com>
2023-01-05 15:06:48 -0500

    Add project files.

---
+++ Controllers/NorrisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChuckNorrisQuotes.Controllers
+{
+    public class NorrisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/NorrisQuote.cs
@@ -0,0 +1,12 @@
+﻿namespace ChuckNorrisQuotes.Models
+{
+    public class NorrisQuote
+    {
+
+
+        public string? icon_url { get; set; }
+        public string?  id { get; set; }
+        public string? url { get; set; }
+        public string? value { get; set; }   
+    }
+}
